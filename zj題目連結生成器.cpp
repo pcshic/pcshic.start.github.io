459b5bdb8292146c02075faca0fb83d65badce99
allem40306 <allem40306@gmail.com>
2016-09-11 17:23:14 +0800

    更新題目選單 發明 zj連結產生器

---
+++ zj題目連結生成器.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string a, b;
+	string x = "<li><a href=\"https://zerojudge.tw/ShowProblem?problemid=";
+	string y = "</a></li>";
+	while (cin >> a){
+		getline(cin, b);
+		cout << x << a << "\">" << a << '.' << b << y << endl;
+	}
+}
