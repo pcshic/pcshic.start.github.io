d55cb4046a9d96be67a520555b50a2899b88cf96
allem40306 <allem40306@gmail.com>
2017-05-14 21:38:38 +0800

    add button in ch01~ch03 and find ch03 lost half qq

---
+++ selectchapter.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	$("#button1").click(function(){
+	  $(".para").hide();
+	  $("#para1").show();
+	});
+	$("#button2").click(function(){
+	  $(".para").hide();
+	  $("#para2").show();
+	});
+	$("#button3").click(function(){
+	  $(".para").hide();
+	  $("#para3").show();
+	});
+	$("#button4").click(function(){
+	  $(".para").hide();
+	  $("#para4").show();
+	});
+	$("#button5").click(function(){
+	  $(".para").hide();
+	  $("#para5").show();
+	});
+});
